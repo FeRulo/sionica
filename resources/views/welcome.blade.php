1ce2001400a3c1369b00ee620795aa0e1f6123a7
Anderson Landazuri <anderson.sionica@gmail.com>
2018-03-22 17:34:12 -0500

    subiendo proyecto funcionarios

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.app')
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>FUNCIONARIOS</h1>
+        <nav>
+            <ul class="nav nav-pills">
+                <li class="nav nav-item"><a class="nav-link"href="/">Home</a></li>
+            </ul>
+        </nav>
+    </div>
+    <img class="thumbnail" src="http://lorempixel.com/600/350" >
+    <input type="text" >
+@endsection
